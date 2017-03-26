4a4f3a527b5f28231692308f2ae7d2db4dfe0330
fahlevireza991 <rezafahlevi.ilkom49@gmail.com>
2017-03-26 22:40:33 +0700

    change total

--- src/components/Total/Total.js
+++ src/components/Total/Total.js
@@ -6,7 +6,7 @@ class Total extends Component {
   render() {
     return (
       <div className="total">
-        Total Pengeluaran: <span className="total-amount">Rp 500000</span>
+        Total Pengeluaran: <span className="total-amount">Rp 1500000</span>
       </div>
     )
   }
